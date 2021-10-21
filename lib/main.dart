b237a101d500bdf2290c38e19722c52697325881
Farzan Hashmi <farzan.ahmad.hashmi@gmail.com>
2021-10-21 00:19:29 -0700

    another one baby

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,7 @@ class HomePage extends StatelessWidget{
                       IconButton(
                         icon: Image.asset('images/resume.png'),
                         onPressed: () {openresume();},
-                        iconSize: 29,
+                        iconSize: 35,
                       ),
 
                     ]
